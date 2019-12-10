b0d6b25d9fd3283fae7ad78dd6e9d140f11c60aa
zhangzhijie <1076916122@qq.com>
2019-12-10 16:42:23 +0800

    初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="content">
+		这是首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
